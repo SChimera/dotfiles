dc5dcc3b5dff06d004737a3ec31a46a4154c0e91
Sebastian Chimera <schimera@schimera.dev>
2026-06-02 18:54:34 +0200

    steam: disable gamescope capSysNice to dodge bwrap setuid breakage
    bubblewrap 0.11.2 (NixOS 26.05) dropped setuid support, so the setuid
    bwrap that capSysNice routes Steam through aborts on launch. Disabling
    capSysNice reverts to the unprivileged store bwrap.
    
    Ref: https://github.com/NixOS/nixpkgs/issues/523200

--- nixos/gaming.nix
+++ nixos/gaming.nix
@@ -9,7 +9,11 @@
 
   programs.gamescope = {
     enable = true;
-    capSysNice = true;
+    # capSysNice routes Steam's FHS sandbox through a *setuid* bwrap, but
+    # bubblewrap 0.11.2 (NixOS 26.05) dropped setuid support, so Steam aborts
+    # with "setuid use of bubblewrap is not supported in this build".
+    # https://github.com/NixOS/nixpkgs/issues/523200
+    capSysNice = false;
   };
 
   programs.gamemode.enable = true;
